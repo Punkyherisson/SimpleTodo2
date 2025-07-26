9fe4f44c25c12755e2370d017a00f12f2ce0548b
Punkyherisson <153446211+Punkyherisson@users.noreply.github.com>
2025-07-26 20:09:14 +0000

    Version 1 Saved in Replit

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
-#Essai de Todo en Python V0.1 ajout de nouvelles taches
+#Essai de Todo en Python V0.1 ajout de nouvelles taches 
 maDate=input("Entrez une date au format jj/mm/aaaa : ")
 #Espagnol
 tacheDuolingo=input("Avez-vous fait Duolingo : ")
-tacheExosDele=input("Avez-vous fait des exercices dele : ")
-tacheNoticias=input("Avez-vous lu les noticias : ")
-tacheMosalingua=input("Avez-vous fait des mosalingua : ")
-tacheCafeynVocable=input("Avez-vous fait un caféyn vocable : ")
+tacheMosalingua=input("Avez-vous fait mosalingua : ")
+tacheCafeynVocable=input("Avez-vous lu un caféyn ou vocable : ")
 tacheMondly=input("Avez-vous fait Mondly : ")
 #Japonais
 tacheKana=input("Avez-vous fait Kana : ")
 tacheDrops=input("Avez-vous fait les drops : ")
-tacheMosaJaponais=input("Avez-vous fait Japonais : ")
-tachekatakanapro=input("Avez-vous fait katakanapro : ")
+tacheMosaJaponais=input("Avez-vous fait Mosa Japonais : ")
+tachekawai=input("Avez-vous fait Kawai : ")
 tacheMinnaNoNihongo=input("Avez-vous fait Minna no Nihongo : ")
-tacheDevoirsJaponais=input("Avez-vous fait des devoirs japonais : ")
 #Python
-tache100days=input("Avez-vous fait 100 jours de code : ")
-tacheCodeAcademy=input("Avez-vous fait Code Academy : ")
-tachePybites=input("Avez-vous fait Pybites : ")
+tacheGithub=input("Avez-vous un commit : ")
 tacheDatacamp=input("Avez-vous fait Datacamp : ")
-tacheKaggle=input("Avez-vous fait Kaggle : ")
-tacherepli=input("Avez-vous fait Repli : ")
+tachetuto=input("Avez-vous fait un tuto : ")
+tachereplit=input("Avez-vous fait du Replit : ")
 tacheRealPython=input("Avez-vous fait Real Python : ")
-tacheProjetPython=input("Avez-vous fait un projet Python : ")
 #Sport/Sante
 tacheSport=input("Avez-vous fait du sport : ")
 tacheApprentissage=input("Avez-vous fait de l'apprentissage : ")
-tacheFruits=input("Avez-vous mangé des fruits : ")
-tacheLegumes=input("Avez-vous mangé des légumes : ")
-tacheProteines=input("Avez-vous mangé des protéines : ")
+tacheDiet=input("Avez-vous noté vos repas : ")
 #Famille
 tacheFamille=input("Avez-vous fait un truc pour la  famille : ")
 
 
 #on affiche les résultats dans le fichier todo
-fichier=open("todo.txt","w")
+#on teste 
+
+fichier=open("todo.txt","a")
 #écrire dans le fichier
 fichier.write("Date : "+maDate+"\n\n")
-fichier.write("Version 01 : "+tacheDuolingo+"\n")
+fichier.write("Version 01 : \n")
 #Espagnol
 fichier.write("Objectifs Espagnol\n")            
 fichier.write("Duolingo : "+tacheDuolingo+"\n")
-fichier.write("Exos dele : "+tacheExosDele+"\n")
-fichier.write("Noticias : "+tacheNoticias+"\n")
 fichier.write("Mosalingua : "+tacheMosalingua+"\n")
 fichier.write("Caféyn vocable : "+tacheCafeynVocable+"\n")
 fichier.write("Mondly : "+tacheMondly+"\n")
@@ -52,31 +44,27 @@ fichier.write("Objectifs Japonais\n")
 fichier.write("Kana : "+tacheKana+"\n")
 fichier.write("Drops : "+tacheDrops+"\n")
 fichier.write("Japonais : "+tacheMosaJaponais+"\n")
-fichier.write("Katakanapro : "+tachekatakanapro+"\n")
+fichier.write("Kawai : "+tachekawai+"\n")
 fichier.write("Minna no Nihongo : "+tacheMinnaNoNihongo+"\n")
-fichier.write("Devoirs japonais : "+tacheDevoirsJaponais+"\n")
 #Python
 fichier.write("Objectifs Python\n")
-fichier.write("100 jours de code : "+tache100days+"\n")
-fichier.write("Code Academy : "+tacheCodeAcademy+"\n")
-fichier.write("Pybites : "+tachePybites+"\n")
+fichier.write("Github Commit : "+tacheGithub+"\n")
 fichier.write("Datacamp : "+tacheDatacamp+"\n")
-fichier.write("Kaggle : "+tacheKaggle+"\n")
-fichier.write("Repli : "+tacherepli+"\n")
+fichier.write("Tuto : "+tachetuto+"\n")
+fichier.write("Repli : "+tachereplit+"\n")
 fichier.write("Real Python : "+tacheRealPython+"\n")
-fichier.write("Projet Python : "+tacheProjetPython+"\n")
 #Sport/Sante
 fichier.write("Objectifs Sport/Sante\n")
 fichier.write("Sport : "+tacheSport+"\n")
 fichier.write("Apprentissage : "+tacheApprentissage+"\n")
-fichier.write("Fruits : "+tacheFruits+"\n")
-fichier.write("Legumes : "+tacheLegumes+"\n")
-fichier.write("Proteines : "+tacheProteines+"\n")
-              
+fichier.write("Regime : "+tacheDiet+"\n")
+             
 #Famille
 fichier.write("Objectifs Famille\n")
 fichier.write("Famille : "+tacheFamille+"\n")
+fichier.write("===================================\n")
 #fermer le fichier
 fichier.close()
+print("Fichier todo.txt mis à jour")
 
               
